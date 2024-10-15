7a3b0d8f84177156190241ad881de2acf9879130
Faisal Ahmed <faisalrajshahi2003@gmail.com>
2024-10-15 06:04:00 +0600

    Add files via upload

---
+++ portfolio/Base/views.py
@@ -0,0 +1,46 @@
+from django.shortcuts import render ,redirect
+from django.http import HttpResponse
+from django.contrib import messages
+from Base import models
+from Base.models import Contact 
+# from django.contrib.auth.decorators import login_required
+def home(request):
+    return render(request,'home.html')
+
+
+# @login_required(login_url='')
+def contact(request):
+   if request.method=="POST":
+       print('post')
+       name=request.POST.get('name')
+       email=request.POST.get('email')
+       number=request.POST.get('number')
+       content=request.POST.get('content')
+       print(name,email,number,content )
+
+       if len(name)>1 and len(name)<30:
+           pass
+       else:
+           messages.error(request,'Length of the name should be greater than 1 and less than 30 words')
+           return render(request,'home.html')
+       
+       if len (email)>1 and len(email)<30:
+           pass
+       else:
+           messages.error(request,'Invaild email try again')
+           return render(request,'home.html')
+       print(len(number))
+       if  len(number)>9 and len(number)<13:
+           pass
+       else:
+           messages.error(request,'Invaild number please try again')
+           return render(request,'home.html')
+       ins = models.Contact(name=name,email=email,content=content,number=number)
+       ins.save()
+       messages.success(request,'Thank You for contacting me!! Your message has been saved')
+       print('Data has been saved to database')
+ 
+   return render(request,'home.html') 
+
+
+  
